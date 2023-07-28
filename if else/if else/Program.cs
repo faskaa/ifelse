01816353f0248d027a4beb932a7589272f90bf0d
faskaa <fazilnb@code.edu.az>
2023-07-28 23:38:06 +0400

    commit initialized

---
+++ if else/if else/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+           
+
+            if (number < 5 )
+            {
+                Console.WriteLine(number - 5);
+            }
+            else
+            {
+                Console.WriteLine(number + 5);
+            }
+        }
+    }
+}
