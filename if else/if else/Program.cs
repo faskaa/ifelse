8ef556f5c8e4a5a9d3a5ead65784aa1880a7a26f
faskaa <fazilnb@code.edu.az>
2023-07-30 15:07:35 +0400

    writeline color changed

--- if else/if else/Program.cs
+++ if else/if else/Program.cs
@@ -1,26 +1,77 @@
-﻿namespace if_else
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace if_else
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int number = 10;
+            //int number = 10;
            
 
-            if (number < 5 )
-            {
-                Console.WriteLine(number - 5);
-            }
-            else
-            {
-                Console.WriteLine(number + 5);
-            }
+            //if (number < 5 )
+            //{
+            //    Console.WriteLine(number - 5);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(number + 5);
+            //}
 
-            string firstName = "Fazil";
-            string secondName = "Bahramov";
+            //string firstName = "Fazil";
+            //string secondName = "Bahramov";
 
-            Console.WriteLine(firstName + " " + secondName);
+            //Console.WriteLine(firstName + " " + secondName);
+
+            //do
+            //{
+            //    Console.WriteLine(number);
+            //    number++;
+            //} while (number > 15 );
+
+            //Console.WriteLine("salam");
+            //Console.Read();
+
+            //for (int i = 0; i < 5; i++)
+            //{
+
+            //    Console.WriteLine(i);
+
+            //}
+
+
+            //int a = -1;
+            //int b = 4;
+            //int c = 6;
+
+            //int res = a + b * c;
+
+            //Console.WriteLine(res);
+
+            //int a =35;
+            //int b = 5;
+            //int c = 7;
+
+            //int res = a + b;
+
+            //if (res == 40)
+            //{
+            //    int res2 = res % c;
+              
+            //    Console.ForegroundColor = ConsoleColor.Blue;
+            //    Console.WriteLine(res2);
+            //}
+
+            int a = 2 ; int b = 15; int c = 6 ; int d = 1 ; int f = 7; int g = 2;
+
+            int res1 = a + b / c * d - f % g;
+
+            Console.Beep();     
             
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(res1);
+
+
 
         }
     }
